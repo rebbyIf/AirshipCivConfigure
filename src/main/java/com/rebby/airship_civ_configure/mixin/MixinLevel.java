c4a569a0f847cd00f093a734c136271d8142cd87
Bunting_chj <chj990707@postech.ac.kr>
2025-11-16 22:53:36 +0900

    Fixed ships flaming nether biome particles. Well, at least when they aren't really on a nether biome. When getting biome for the blockPos in shipyard, the blockPos is transformed to world coords before getting the biome.

---
+++ src/main/java/com/rebby/airship_civ_configure/mixin/MixinLevel.java
@@ -0,0 +1,22 @@
+package com.rebby.airship_civ_configure.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+@Mixin(Level.class)
+public abstract class MixinLevel implements LevelReader {
+    @Shadow
+    public abstract BiomeManager getBiomeManager();
+
+    @Override
+    public Holder<Biome> getBiome(BlockPos pPos) {
+        return getBiomeManager().getBiome(BlockPos.containing(VSGameUtilsKt.toWorldCoordinates(Level.class.cast(this), pPos.getCenter())));
+    }
+}
