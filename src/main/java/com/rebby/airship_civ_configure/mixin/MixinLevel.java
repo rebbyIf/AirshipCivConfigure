3cb9cd4f0e18ec0bedc1f0ff14c7d63cef7e26a5
Bunting_chj <chj990707@postech.ac.kr>
2025-11-17 12:25:21 +0900

    Arrows can land on a ship. This is a feature from VS2.3 beta 10+, and ported down.

--- src/main/java/com/rebby/airship_civ_configure/mixin/MixinLevel.java
+++ src/main/java/com/rebby/airship_civ_configure/mixin/MixinLevel.java
@@ -10,6 +10,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
+/**
+ * Biome on ship will follow the biome the ship is in, not the shipyard.
+ */
 @Mixin(Level.class)
 public abstract class MixinLevel implements LevelReader {
     @Shadow
