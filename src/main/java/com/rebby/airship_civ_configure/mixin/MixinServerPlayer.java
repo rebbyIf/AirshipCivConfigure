766a6a25dcdddeb26ec7007723307cca3f76f4b4
Bunting_chj <chj990707@postech.ac.kr>
2025-11-28 14:54:56 +0900

    Smoothened the dismounting location on ship.

--- src/main/java/com/rebby/airship_civ_configure/mixin/MixinServerPlayer.java
+++ src/main/java/com/rebby/airship_civ_configure/mixin/MixinServerPlayer.java
@@ -1,5 +1,8 @@
 package com.rebby.airship_civ_configure.mixin;
 
+import com.bawnorton.mixinsquared.TargetHandler;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
@@ -8,13 +11,16 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.eureka.EurekaMod;
 
-@Mixin(ServerPlayer.class)
+@Mixin(value = ServerPlayer.class, priority = 1500)
 public abstract class MixinServerPlayer extends Player {
 
     public MixinServerPlayer(Level pLevel, BlockPos pPos, float pYRot, GameProfile pGameProfile) {
@@ -28,5 +34,20 @@ public abstract class MixinServerPlayer extends Player {
     private void emergencyKit(ServerPlayer pThat, boolean pKeepEverything, CallbackInfo ci){
         addItem(new ItemStack(ForgeRegistries.ITEMS.getValue(ResourceLocation.fromNamespaceAndPath(EurekaMod.MOD_ID, "oak_ship_helm")), 1));
         addItem(new ItemStack(ForgeRegistries.ITEMS.getValue(ResourceLocation.fromNamespaceAndPath(EurekaMod.MOD_ID, "balloon")), 2));
+    }
+
+    @TargetHandler(
+            mixin = "org.valkyrienskies.mod.mixin.server.command.level.MixinServerPlayer",
+            name = "beforeDismountTo"
+    )
+    @Redirect(
+            method = "@MixinSquared:Handler",
+            at = @At(value = "INVOKE", target = "Lorg/valkyrienskies/mod/common/VSGameUtilsKt;toWorldCoordinates(Lorg/valkyrienskies/core/api/ships/Ship;DDD)Lorg/joml/Vector3d;"),
+            remap = false
+    )
+    private Vector3d teleportAddSpeed(Ship ship, double x, double y, double z){
+        Vector3d prevPos = ship.getPrevTickTransform().getShipToWorld().transformPosition(x, y, z, new Vector3d());
+        Vector3d curPos = ship.getTransform().getShipToWorld().transformPosition(x, y, z, new Vector3d());
+        return curPos.mul(2).sub(prevPos);
     }
 }
